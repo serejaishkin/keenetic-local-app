ab3b86bb624ffcdc93b80b197eb523c199e76dfb
serejaishkin <47511319+serejaishkin@users.noreply.github.com>
2026-07-21 00:02:23 +0300

    Update TerminalScreen.kt

--- app/src/main/java/com/keenetic/local/ui/screens/TerminalScreen.kt
+++ app/src/main/java/com/keenetic/local/ui/screens/TerminalScreen.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.keenetic.local.ui.RouterViewModel
 import com.keenetic.local.ui.theme.KeeneticColors
 
